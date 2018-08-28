ce121c1bc1f599b3378a9361260183b9590e3eaa
DESKTOP-72590BA\user <cruzleedan@gmail.com>
2018-08-28 12:28:23 +0800

    facebook login welcome email

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,8 +1,9 @@
 const { ExtractJwt, Strategy } = require('passport-jwt');
 const FacebookTokenStrategy = require('passport-facebook-token');
-const { User }      = require('../models');
-const CONFIG        = require('../config');
-const {to}          = require('../services/util.service');
+const { User, Sequelize } = require('../models');
+const Op = Sequelize.Op;
+const CONFIG = require('../config');
+const { to, ReE, ReS } = require('../services/util.service');
 
 module.exports = function(passport){
     var opts = {};
@@ -24,29 +25,47 @@ module.exports = function(passport){
 
 
     passport.use(new FacebookTokenStrategy({
-        clientID: CONFIG.fb_client_id,
-        clientSecret: CONFIG.fb_client_secret
-    }, async (accessToken, refreshToken, profile, done) => {
-        let err, user;
-        [err, user] = await to (
-            User.findOrCreate({where: {facebookId: profile.id}, defaults: {
-                facebook_id: profile.id,
-                auth_method: 'facebook',
-                email: profile.emails[0].value
-            }})
-            .spread((user, created) => {
-                return user;
-            })
-        );
-        console.log('isError', !!(err));
-        console.log('isUser', !!(user));
-        if(err) return done(err, false);
-        if(user) {
-            return done(null, user);
-        }else{
-            return done(null, false);
-        }
-    }));
+            clientID: CONFIG.fb_client_id,
+            clientSecret: CONFIG.fb_client_secret
+        }, async (accessToken, refreshToken, profile, done) => {
+            let error, user;
+            [error, user] = await to (
+                User.findOne({
+                    where: {
+                        email: profile.emails[0].value,
+                        facebookId: {
+                            [Op.or]: [
+                                {
+                                    [Op.ne]: profile.id
+                                }, 
+                                null 
+                            ]
+                        }
+                    }
+                })
+            );
+            if(error) return done(error, false);
+            if(user) return done({'error': 'Email already registered, please login using your email instead.'}, false);
+            [err, user] = await to (
+                User.findOrCreate({where: {facebookId: profile.id}, defaults: {
+                    facebook_id: profile.id,
+                    auth_method: 'facebook',
+                    email: profile.emails[0].value
+                }})
+                .spread((user, created) => {
+                    return user;
+                })
+            );
+            console.log('isError', !!(err));
+            console.log('isUser', !!(user));
+            if(error) return done(error, false);
+            if(user) {
+                return done(null, user);
+            }else{
+                return done(null, false);
+            }
+        })
+    );
 }
 
 
